5e0c965020b3ae137fc6298c29522fc622932d02
jesug <jesugalabtf@gmail.com>
2025-02-08 22:26:37 +0100

    Modificado el menu superior añadiendo un selector para consultas. Por Rol

--- keys.js
+++ keys.js
@@ -1,5 +1,6 @@
 module.exports = {
   mongodb: {
-    URI: 'mongodb+srv://Jesus:1234@proyectogrupojs.k9xjy.mongodb.net/'
+    URI: 'mongodb://localhost:27017/login-node'
+  //  URI: 'mongodb+srv://Jesus:1234@proyectogrupojs.k9xjy.mongodb.net/'
   }
 };
