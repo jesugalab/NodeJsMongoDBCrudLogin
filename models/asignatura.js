ce3c648e10a0dabdabc02907879d09cf0c5bdc39
Bokiron <davidconnana@gmail.com>
2025-02-13 19:23:22 +0100

    Cada usuario puede ver sus asignaturas

--- models/asignatura.js
+++ models/asignatura.js
@@ -64,6 +64,94 @@ AsignaturaSchema.methods.findSearch = async function (search, usuario) {
     .then(result => result)
     .catch(error => console.log(error));
 };
+AsignaturaSchema.statics.findByUser = async function(userId, userRole) {
+  let asignaturas;
 
+  if (userRole.toLowerCase() === 'profesor') {
+      asignaturas = await this.find({
+          listaProfesores: userId
+      }).lean();
+  } else if (userRole.toLowerCase() === 'alumno') {
+      asignaturas = await this.find({
+          listaAlumnos: userId
+      }).lean();
+  } else {
+      return [];
+  }
+
+  // Obtener los IDs de los estudios relacionados
+  const estudioIds = asignaturas.map(asignatura => asignatura.estudio_id);
+
+  // Obtener los datos de los estudios
+  const Estudio = mongoose.model('listaEstudio');
+  const estudios = await Estudio.find({
+      _id: {
+          $in: estudioIds
+      }
+  }).lean();
+
+  // Crear un mapa de estudios por ID
+  const estudiosMap = {};
+  estudios.forEach(estudio => {
+      estudiosMap[estudio._id] = estudio;
+  });
+
+  // Obtener los IDs de los alumnos y profesores relacionados
+  let alumnoIds = [];
+  let profesorIds = [];
+
+  asignaturas.forEach(asignatura => {
+      alumnoIds = alumnoIds.concat(asignatura.listaAlumnos);
+      profesorIds = profesorIds.concat(asignatura.listaProfesores);
+  });
+
+  // Eliminar IDs duplicados
+  alumnoIds = [...new Set(alumnoIds)];
+  profesorIds = [...new Set(profesorIds)];
+
+  // Obtener los datos de los alumnos y profesores
+  const Usuario = mongoose.model('listaUsuario');
+  const alumnos = await Usuario.find({
+      _id: {
+          $in: alumnoIds
+      }
+  }).lean();
+  const profesores = await Usuario.find({
+      _id: {
+          $in: profesorIds
+      }
+  }).lean();
+
+  // Crear mapas de alumnos y profesores por ID
+  const alumnosMap = {};
+  alumnos.forEach(alumno => {
+      alumnosMap[alumno._id] = alumno;
+  });
+  const profesoresMap = {};
+  profesores.forEach(profesor => {
+      profesoresMap[profesor._id] = profesor;
+  });
+
+  // Reemplazar los IDs con los objetos correspondientes
+  const asignaturasConDatos = asignaturas.map(asignatura => ({
+      ...asignatura,
+      estudio: estudiosMap[asignatura.estudio_id] || {
+          nombre: "No encontrado",
+          tipo: "-"
+      },
+      listaAlumnos: asignatura.listaAlumnos.map(id => alumnosMap[id] || {
+          nombre: "No encontrado",
+          apellidos: "",
+          email: ""
+      }),
+      listaProfesores: asignatura.listaProfesores.map(id => profesoresMap[id] || {
+          nombre: "No encontrado",
+          apellidos: "",
+          email: ""
+      })
+  }));
+
+  return asignaturasConDatos;
+};
 
 module.exports = mongoose.model('listaAsignatura', AsignaturaSchema);
